bf05dfcb85d856578d8838d36ad25bd89e0288e7
yay <amy.lian314159@gmail.com>
2026-02-17 09:22:49 +1300

    flask code, tested with sql query

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, g
 
+import sqlite3
+
+DATABASE = 'database.db'
+
+
+#initialize app
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def home():
     #home page
-    return "hello world"
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM books;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return str(results)
 
 if __name__ == "__main__": 
     app.run(debug=True)
